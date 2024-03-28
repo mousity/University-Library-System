2404d77064dd44297e92a9505f45f741375a5e7c
Christopher <chrisnieves60@gmail.com>
2024-03-28 17:57:51 -0400

    book data

--- client/University-Library/src/pages/Homepage.jsx
+++ client/University-Library/src/pages/Homepage.jsx
@@ -2,24 +2,29 @@ import react, { useEffect, useState } from "react";
 import './Homepage.css';
 import { createClient } from "@supabase/supabase-js";
 function Homepage() {
+
+    //SUPABASE CONNECTION DATA
     const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
     const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
     const supabase = createClient(supabaseUrl, supabaseKey)
+    //SUPABASE CONNECTION DATA
 
     const [countries, setCountries] = useState([]);
-
+    
     useEffect(() => {
       getCountries();
+      
     }, []);
-
+    
     async function getCountries() {
-      const { data } = await supabase.from("testing").select();
+      const { data } = await supabase.from("books").select();
       setCountries(data);
     }
+    console.log(countries)
     return (
       <ul>
         {countries.map((country) => (
-          <li key={country.name}>{country.name}</li>
+          <li key={country.id}>{country.title}</li>
         ))}
       </ul>
 
