469772455c405e6fcc0603f4013d71dd5dc37652
Samr Mouna <samr.mouna@gmail.com>
2024-05-14 22:19:42 -0400

    stash

--- client/University-Library/src/main.jsx
+++ client/University-Library/src/main.jsx
@@ -7,13 +7,10 @@ import './index.css';
 import SignupPage from "./pages/Signup.jsx";
 import LoginPage from "./pages/Login.jsx";
 import Books from "./pages/Books.jsx";
-<<<<<<< HEAD
 import Settings from "./pages/Settings.jsx";
-=======
 import Events from "./pages/Events.jsx";
 import Faq from "./pages/Faq.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
->>>>>>> 08a422171fa66327f0fa3a41c8187f8af1bc3300
 import { AuthProvider } from "../../AuthContext.jsx";
 import Mybooks from "./pages/Mybooks.jsx";
 const router = createBrowserRouter([
